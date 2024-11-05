54b0688f338813b0fde3a2785ee4f6172a9495ca
AerospikeNate-L <nlarsen@aerospike.com>
2024-11-05 15:44:31 -0800

    adjust pom.xml per Lyndon's recs, add print statements

--- examples/graph_example/src/main/java/aerospike/com/Main.java
+++ examples/graph_example/src/main/java/aerospike/com/Main.java
@@ -10,22 +10,26 @@ import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalS
 
 
 public class Main {
-    private static final String HOST = "localhost";
+    private static final String HOST = "172.18.0.3";
     private static final int PORT = 8182;
     private static final Cluster.Builder BUILDER = Cluster.build().addContactPoint(HOST).port(PORT).enableSsl(false);
+
     public static void main(String[] args) {
         final Cluster cluster = BUILDER.create();
-
+        System.out.println("CONNECTING TO GRAPH");
         final GraphTraversalSource g = traversal().withRemote(DriverRemoteConnection.using(cluster));
-
+        
+        System.out.println("ADDING VERTEX:= FOO");
         g.addV("foo")
             .property("company", "aerospike")
             .property("scale","unlimited")
             .iterate();
 
+        System.out.println("READING BACK DATA..");
         Vertex ReadVertex = g.V().has("company","aerospike").next();
 
         long airportCount = g.V().hasLabel("airport").count().next();
         long flightCount =  g.V().has("code","SFO").outE().count().next();
+        System.out.println("DONE!");
     }
 }
