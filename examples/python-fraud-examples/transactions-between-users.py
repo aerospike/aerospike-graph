58716d21d4953a4e7e0614affeb881b0a3ef002d
Connor Hengstler <hengstler2005@gmail.com>
2025-04-21 10:30:40 -0700

    Changed directory name, transactions file name, requirements to a md, and removed asyncio as it is unused in transactions-betweenusers.py

--- examples/python-fraud-examples/transactions-between-users.py
+++ examples/python-fraud-examples/transactions-between-users.py
@@ -1,7 +1,8 @@
+
 import random
 import datetime
 import traceback
-import asyncio
+
 from gremlin_python.process.graph_traversal import __
 from gremlin_python.process.traversal import T, Direction
 from gremlin_python.process.anonymous_traversal import traversal
